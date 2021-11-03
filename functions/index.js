4fc667f3e3ce09a787a70d33c38dba4f512b12c1
Thayaruban Thayalan <65121546+Thayaruban@users.noreply.github.com>
2021-11-03 05:30:22 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ admin.initializeApp();
 
 //Handlers
 const handleGetPreferedDestinations = require("./handlers/getPreferedDestinations");
+const handleAddUser = require("./handlers/addUser");
 
 //End Points
 exports.getPreferedDestinations = handleGetPreferedDestinations;
+exports.addUser = handleAddUser;
